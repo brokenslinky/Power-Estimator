d602cd653e878f1bc836f8c157092a252b80e91f
Brock Palmer <brokenslinky@gmail.com>
2020-01-02 19:53:59 -0700

    Add summaries.

--- Curve.cs
+++ Curve.cs
@@ -2,32 +2,53 @@
 
 namespace Power_Estimator
 {
+    /// <summary>
+    /// Represents a 1:1 lookup table with linear interpolation.
+    /// </summary>
     public class Curve
     {
+        /// <summary>
+        /// Values of the lookup inputs.
+        /// </summary>
         public double[] x;
+        /// <summary>
+        /// Values of the lookup outputs.
+        /// </summary>
         public double[] y;
-        public double Interpolate(double x_value)
+
+        /// <summary>
+        /// Linear interpolation of this curve for the given input value.
+        /// </summary>
+        /// <param name="xValue">The value of the input parameter.</param>
+        /// <returns>The interpolated output value.</returns>
+        public double Interpolate(double xValue)
         {
-            int index_low = 0;
-            bool exact_x = false;
+            int indexLow = 0;
+            bool exactX = false;
             for (int index = 0; index < x.Length - 1; index++)
             {
-                if (x_value == x[index])
+                if (xValue == x[index])
                 {
-                    exact_x = true;
-                    index_low = index;
+                    exactX = true;
+                    indexLow = index;
                     break;
                 }
-                if ((x[index] < x_value && x[index + 1] > x_value) || (x[index] > x_value && x[index + 1] < x_value))
-                    index_low = index;
+                if ((x[index] < xValue && x[index + 1] > xValue) || (x[index] > xValue && x[index + 1] < xValue))
+                    indexLow = index;
             }
-            double dydx = (y[index_low + 1] - y[index_low]) / (x[index_low + 1] - x[index_low]);
-            double y_value = y[index_low];
-            if (!exact_x)
-                y_value += dydx * (x_value - x[index_low]);
+            double dydx = (y[indexLow + 1] - y[indexLow]) / (x[indexLow + 1] - x[indexLow]);
+            double y_value = y[indexLow];
+            if (!exactX)
+                y_value += dydx * (xValue - x[indexLow]);
             return y_value;
         }
 
+        /// <summary>
+        /// Parse a Curve from two strings corresponding to the x and y arrays.
+        /// </summary>
+        /// <param name="x">List of input values (comma, space, or line separated).</param>
+        /// <param name="y">List of output values (comma, space, or line separated).</param>
+        /// <returns>A Curve based on the supplied values.</returns>
         static public Curve Read_curve(string x, string y)
         {
             Curve curve = new Curve();
@@ -36,7 +57,7 @@ namespace Power_Estimator
             int index = 0;
             foreach (char c in x)
             {
-                if (c == '\n')
+                if (c == '\n' || c == ' ' || c == ',' || c == '\t')
                 {
                     buffer[index] = Convert.ToDouble(word);
                     word = string.Empty;
--- Table.cs
+++ Table.cs
@@ -3,68 +3,91 @@ using System.IO;
 
 namespace Power_Estimator
 {
-        public class Table
+    /// <summary>
+    /// Represents a 2:1 lookup table with linear interpolation.
+    /// </summary>
+    public class Table
+    {
+        /// <summary>
+        /// Values in the 'x' axis of the lookup table.
+        /// </summary>
+        public double[] x;
+        /// <summary>
+        /// Values in the 'y' axis of the lookup table.
+        /// </summary>
+        public double[] y;
+        /// <summary>
+        /// Output values at the input nodes [x,y].
+        /// </summary>
+        public double[,] value;
+
+        /// <summary>
+        /// Linear interpolation of this table for the given input values.
+        /// </summary>
+        /// <param name="xValue">The value of the 'x' input parameter.</param>
+        /// <param name="yValue">The value of the 'y' input parameter.</param>
+        /// <returns>The interpolated output value.</returns>
+        public double Interpolate(double xValue, double yValue)
         {
-            public double[] x;
-            public double[] y;
-            public double[,] value;
-            public double Interpolate(double x_value, double y_value)
+            int x_index_low = 0;
+            int y_index_low = 0;
+            bool exact_x = false;
+            bool exact_y = false;
+            for (int x_index = 0; x_index < x.Length - 1; x_index++)
             {
-                int x_index_low = 0;
-                int y_index_low = 0;
-                bool exact_x = false;
-                bool exact_y = false;
-                for (int x_index = 0; x_index < x.Length - 1; x_index++)
+                if (xValue == x[x_index])
                 {
-                    if (x_value == x[x_index])
-                    {
-                        exact_x = true;
-                        x_index_low = x_index;
-                        break;
-                    }
-                    if ((x[x_index] < x_value && x[x_index + 1] > x_value) || (
-                        x[x_index] > x_value && x[x_index + 1] < x_value))
-                        x_index_low = x_index;
+                    exact_x = true;
+                    x_index_low = x_index;
+                    break;
                 }
-                for (int y_index = 0; y_index < y.Length - 1; y_index++)
-                {
-                    if (y_value == y[y_index])
-                    {
-                        exact_y = true;
-                        y_index_low = y_index;
-                        break;
-                    }
-                    if ((y[y_index] < y_value && y[y_index + 1] > y_value) || (
-                        y[y_index] > y_value && y[y_index + 1] < y_value))
-                        y_index_low = y_index;
-                }
-                if (y_value < y[0])
-                    y_value = y[0];
-                if (y_value > y[y.Length - 1])
-                    y_value = y[y.Length - 1];
-                if (x_value < x[0])
-                    x_value = x[0];
-                if (x_value > x[x.Length - 1])
-                    x_value = x[x.Length - 1];
-                double interpolated_value = 0.0;
-                double x_span = Math.Abs(x[x_index_low + 1] - x[x_index_low]);
-                double y_span = Math.Abs(y[y_index_low + 1] - y[y_index_low]);
-                double enclosed_area = x_span * y_span;
-                for (int x_index = x_index_low; x_index < x_index_low + 2; x_index++)
-                    for (int y_index = y_index_low; y_index < y_index_low + 2; y_index++)
-                        interpolated_value += value[x_index, y_index] * 
-                            (x_span - Math.Abs(x_value - x[x_index])) * (y_span - Math.Abs(y_value - y[y_index]));
-                interpolated_value /= enclosed_area;
-                return interpolated_value;
+                if ((x[x_index] < xValue && x[x_index + 1] > xValue) || (
+                    x[x_index] > xValue && x[x_index + 1] < xValue))
+                    x_index_low = x_index;
             }
+            for (int y_index = 0; y_index < y.Length - 1; y_index++)
+            {
+                if (yValue == y[y_index])
+                {
+                    exact_y = true;
+                    y_index_low = y_index;
+                    break;
+                }
+                if ((y[y_index] < yValue && y[y_index + 1] > yValue) || (
+                    y[y_index] > yValue && y[y_index + 1] < yValue))
+                    y_index_low = y_index;
+            }
+            if (yValue < y[0])
+                yValue = y[0];
+            if (yValue > y[y.Length - 1])
+                yValue = y[y.Length - 1];
+            if (xValue < x[0])
+                xValue = x[0];
+            if (xValue > x[x.Length - 1])
+                xValue = x[x.Length - 1];
+            double interpolated_value = 0.0;
+            double x_span = Math.Abs(x[x_index_low + 1] - x[x_index_low]);
+            double y_span = Math.Abs(y[y_index_low + 1] - y[y_index_low]);
+            double enclosed_area = x_span * y_span;
+            for (int x_index = x_index_low; x_index < x_index_low + 2; x_index++)
+                for (int y_index = y_index_low; y_index < y_index_low + 2; y_index++)
+                    interpolated_value += value[x_index, y_index] * 
+                        (x_span - Math.Abs(xValue - x[x_index])) * (y_span - Math.Abs(yValue - y[y_index]));
+            interpolated_value /= enclosed_area;
+            return interpolated_value;
+        }
 
-
-        static public Table Read_table(string table_location)
+        /// <summary>
+        /// Parse a Table from a tab delimited file.
+        /// </summary>
+        /// <param name="tableLocation">Path to the file to parse as a Table.</param>
+        /// <returns>A new Table based on the input file.</returns>
+        static public Table Read_table(string tableLocation)
         {
             Table table = new Table();
             double[] x_buffer = new double[64];
             double[] y_buffer = new double[64];
-            StreamReader reader = new StreamReader(table_location);
+            StreamReader reader = new StreamReader(tableLocation);
             while (reader.Peek() != '\t')
                 reader.ReadLine();
             string word = string.Empty;
